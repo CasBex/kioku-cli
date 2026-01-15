c4137c9f03af824add3d770a6a26f03bef2a0354
Cas Bex <cas.bex@kuleuven.be>
2026-01-15 02:16:06 +0100

    Read word list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io::{self, BufRead};
+
+#[derive(Debug)]
+enum WordlistErr {
+    FileErr(io::Error),
+    NotWordList,
+}
+
+impl From<io::Error> for WordlistErr {
+    fn from(value: io::Error) -> Self {
+        Self::FileErr(value)
+    }
+}
+
+fn parse_wordlist(filename: &str) -> Result<Vec<String>, WordlistErr> {
+    io::BufReader::new(fs::File::open(filename)?)
+        .lines()
+        .map(|x| {
+            x.map_err(WordlistErr::from).and_then(|y| {
+                let ty = y.trim();
+                if ty.contains(char::is_whitespace) {
+                    Err(WordlistErr::NotWordList)
+                } else {
+                    Ok(String::from(ty))
+                }
+            })
+        })
+        .collect()
+}
+
+fn main() -> Result<(), WordlistErr> {
+    let args: Vec<String> = std::env::args().collect();
+    let filepath = &args[1];
+    let wordlist = parse_wordlist(filepath)?;
+    println!("Read wordlist of length {}", wordlist.len());
+    Ok(())
 }
