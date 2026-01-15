2b66d521b881f481f7e67a6a0ee3da948667ab7a
Cas Bex <cas.bex@kuleuven.be>
2026-01-15 04:21:29 +0100

    Timestamp files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use chrono::prelude::*;
 use rand::prelude::*;
+use std::fmt;
 use std::fs;
 use std::io::{self, BufRead};
 
@@ -8,10 +10,28 @@ enum WordlistErr {
     NotWordList,
 }
 
+impl fmt::Display for WordlistErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::FileErr(e) => e.fmt(f),
+            Self::NotWordList => write!(f, "Wordlist badly formatted"),
+        }
+    }
+}
+
+impl std::error::Error for WordlistErr {}
+
 impl From<io::Error> for WordlistErr {
     fn from(value: io::Error) -> Self {
         Self::FileErr(value)
     }
+}
+
+#[derive(serde::Serialize)]
+struct MetaData {
+    label: String,
+    revision: Option<String>,
+    timestamp: String,
 }
 
 fn parse_wordlist(filename: &str) -> Result<Vec<String>, WordlistErr> {
@@ -44,10 +64,42 @@ fn generate_name<'a>(wordlist: &'a Vec<String>, num_words: usize) -> String {
     output
 }
 
-fn main() -> Result<(), WordlistErr> {
+fn generate_metadata(filename: &str, slug: &str, use_utc: bool) -> Result<(), io::Error> {
+    let revision = git2::Repository::discover(".").ok().and_then(|rep| {
+        rep.head()
+            .ok()
+            .and_then(|head| head.target())
+            .map(|oid| oid.to_string())
+    });
+    let timestamp = chrono::Local::now().to_rfc3339();
+    let meta = MetaData {
+        label: slug.to_string(),
+        revision,
+        timestamp,
+    };
+    let mut opener = std::fs::OpenOptions::new();
+    opener.create(true);
+    if filename.ends_with(".jsonl") {
+        opener.append(true);
+    } else {
+        opener.write(true);
+    }
+    let writer = if filename.ends_with(".jsonl") || filename.ends_with(".json") {
+        io::BufWriter::new(opener.open(filename)?)
+    } else {
+        let mut tmp = String::from(filename);
+        tmp.push_str(".json");
+        io::BufWriter::new(opener.open(tmp)?)
+    };
+    serde_json::to_writer_pretty(writer, &meta).unwrap();
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     let filepath = &args[1];
     let wordlist = parse_wordlist(filepath)?;
+    // generate_metadata("tmp.jsonl", "test", true)?;
     println!("{}", generate_name(&wordlist, 3));
     Ok(())
 }
