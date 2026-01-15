f1ca04ae98e4031b262b5b54129fe4b4a5fafa77
Cas Bex <cas.bex@kuleuven.be>
2026-01-15 02:27:26 +0100

    Generate name from word list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::prelude::*;
 use std::fs;
 use std::io::{self, BufRead};
 
@@ -29,10 +30,24 @@ fn parse_wordlist(filename: &str) -> Result<Vec<String>, WordlistErr> {
         .collect()
 }
 
+fn generate_name<'a>(wordlist: &'a Vec<String>, num_words: usize) -> String {
+    let mut rng = rand::rng();
+    let mut output = String::new();
+    for word in (0..num_words).map(|_| wordlist[rng.random_range(0..wordlist.len())].as_str()) {
+        if output.len() == 0 {
+            output.push_str(word);
+        } else {
+            output.push('-');
+            output.push_str(word);
+        }
+    }
+    output
+}
+
 fn main() -> Result<(), WordlistErr> {
     let args: Vec<String> = std::env::args().collect();
     let filepath = &args[1];
     let wordlist = parse_wordlist(filepath)?;
-    println!("Read wordlist of length {}", wordlist.len());
+    println!("{}", generate_name(&wordlist, 3));
     Ok(())
 }
